3b4d3cd772b04ecb4643a0506ddb9cfba0910ba6
pyama86 <pyama@pepabo.com>
2016-08-12 19:36:17 +0900

    bump to 0.3.0

--- stns_passwd.rb
+++ stns_passwd.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
-HOMEBREW_STNS_PASSWD_VERSION='0.2.0'
+HOMEBREW_STNS_PASSWD_VERSION='0.3.0'
 class StnsPasswd < Formula
   homepage 'https://github.com/STNS/stns-passwd'
   url "https://github.com/STNS/stns-passwd/releases/download/#{HOMEBREW_STNS_PASSWD_VERSION}/stns_darwin_amd64.zip"
-  sha256 'd1bac8ac45f784e31f768cd3b18a6214992dc6aaea234b0f31cc86af4ee7d6f9'
+  sha256 '63f3e76afa2df707ded3b76974dca97b0c55304f96aab7ec66ccf6a9903ae46c'
   version HOMEBREW_STNS_PASSWD_VERSION
   head 'https://github.com/STNS/stns-passwd.git', :branch => 'master'
 
